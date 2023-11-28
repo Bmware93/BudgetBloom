e6127ae62dd72330ae05c81ae10e9764f1e37bbe
Benia Morgan <bmorganware@gmail.com>
2023-11-28 10:58:35 -0500

    Initial Commit

---
+++ SwiftDataTesting/SwiftDataTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTestingApp.swift
+//  SwiftDataTesting
+//
+//  Created by Benia Morgan-Ware on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
