99994e3302a433880e9706af9ccc9d6dda3b6542
Benia Morgan <bmorganware@gmail.com>
2023-12-04 20:45:42 -0500

    Imported swift data for persisted data management expenses are queried and fetched by expense date context has been inserted into enviroment new expense is added to array and saved to context

--- SwiftDataTesting/ContentView.swift
+++ SwiftDataTesting/ContentView.swift
@@ -6,19 +6,119 @@
 //
 
 import SwiftUI
+import SwiftData
+
 
 struct ContentView: View {
+    @State private var isItemSheetShowing = false
+    
+    //injecting context into the content view so it will have access to it
+    @Environment(\.modelContext) var context
+    
+    //fetches the saved data from the context in the add expense sheet
+    @Query(sort: \Expense.date) 
+    var expenses: [Expense]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                List {
+                    ForEach(expenses) { expense in
+                        ExpenseCell(expense: expense)
+                    }
+                    //code that allows us to delete an expense item
+                    //Swipe to delete comes with on delete method
+                    .onDelete { indexSet in
+                        for index in indexSet {
+                            context.delete(expenses[index])
+                        }
+                    }
+                }
+                .navigationTitle("Expenses")
+                .navigationBarTitleDisplayMode(.large)
+                .sheet(isPresented: $isItemSheetShowing) { AddExpenseSheet() }
+                .toolbar {
+                    if !expenses.isEmpty {
+                        Button("Add Expense", systemImage: "plus") {
+                            isItemSheetShowing.toggle()
+                        }
+                    }
+                }
+                .overlay {
+                    if expenses.isEmpty {
+                        ContentUnavailableView(label: {
+                            Label("No Expenses", systemImage: "list.bullet.rectangle.portrait")
+                        },description: {
+                            Text("Start adding expenses to see your list")
+                        }, actions: {
+                            Button("Add Expense") {
+                                isItemSheetShowing.toggle()
+                            }
+                        })
+                        .offset(y: -60)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+
+
+#Preview { ContentView()}
+
+struct ExpenseCell: View {
+    
+    let expense: Expense
+    
+    var body: some View {
+        HStack {
+            Text(expense.date, format: .dateTime.month(.abbreviated).day())
+                .frame(width: 70, alignment: .leading)
+            Text(expense.name)
+            Spacer()
+            Text(expense.value, format: .currency(code: "USD"))
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct AddExpenseSheet: View {
+    
+    //Inserting the model context into the expense sheet view
+    @Environment(\.modelContext) var context
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var name: String = ""
+    @State private var date: Date = .now
+    @State private var value: Double = 0
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Expense Name", text: $name)
+                DatePicker("Date", selection: $date, displayedComponents: .date)
+                TextField("Value", value: $value, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("New Expense")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel"){ dismiss() }
+                }
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button("Save") {
+                        let expense = Expense(name: name, date: date, value: value)
+                        //Inserts data in the context container
+                        context.insert(expense)
+                        
+                        dismiss()
+                    }
+                }
+                
+            }
+
+        }
+    }
 }
+
+
+
+
