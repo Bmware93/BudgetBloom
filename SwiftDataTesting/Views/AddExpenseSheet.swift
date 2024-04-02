0ca2dd0864cbcac3578bbccb4d65c7b178584fce
Benia Morgan <bmorganware@gmail.com>
2024-04-01 21:08:35 -0400

    Add spend category section Add space to add a spending category for user expense

--- SwiftDataTesting/Views/AddExpenseSheet.swift
+++ SwiftDataTesting/Views/AddExpenseSheet.swift
@@ -17,10 +17,25 @@ struct AddExpenseSheet: View {
     @State private var name: String = ""
     @State private var date: Date = .now
     @State private var value: Double = 0.0
+    @State private var spendingCategory:SpendingCategory = .clothing
     
     //Disabled add expense button until all data is entered
     var isFormValid: Bool {
         return !name.isEmpty && value > 0
+    }
+    
+    enum SpendingCategory:String, CaseIterable,Identifiable {
+        case housing = "Housing"
+        case utilities = "Utilities"
+        case subcription = "Subsciption"
+        case clothing = "Clothing"
+        case debt = "Debt"
+        case health = "Health"
+        case food = "Food"
+        case entertainment = "Entertainment"
+        case misc = "Misc"
+        
+        var id: String { rawValue }
     }
     
     var body: some View {
@@ -30,6 +45,11 @@ struct AddExpenseSheet: View {
                 DatePicker("Date", selection: $date, displayedComponents: .date)
                 TextField("Value", value: $value, format: .currency(code: "USD"))
                     .keyboardType(.decimalPad)
+                Picker("Category", selection: $spendingCategory){
+                    ForEach(SpendingCategory.allCases, id: \.self) { option in
+                        Text(option.rawValue)
+                    }
+                }
             }
             .navigationTitle("New Expense")
             .navigationBarTitleDisplayMode(.inline)
