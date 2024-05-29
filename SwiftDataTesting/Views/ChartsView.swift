051c2fb968b512d7331ba4a82305b6501f8e5e9b
Benia Morgan <bmorganware@gmail.com>
2024-05-29 11:39:49 -0400

    Donut Chart Refactor Moved the donut chart to seperate swift file.

--- SwiftDataTesting/Views/ChartsView.swift
+++ SwiftDataTesting/Views/ChartsView.swift
@@ -201,46 +201,7 @@ struct ChartsView: View {
 }
 
 
-struct DonutChartView: View {
-    let categoryTotals: [CategoryTotal]
-    @State private var isAnimating = false
-    
-    var body: some View {
-        Chart(categoryTotals) { item in
-            SectorMark(angle: .value("Total Spent",
-                                     isAnimating == false ? 0 : item.total ),
-                       innerRadius: .ratio(0.5))
-            
-            .position(by: .value("Category", item.category.rawValue))
-            .foregroundStyle(by: .value("Category", item.category.rawValue))
-            .position(by: .value("Value", item.total))
-            
-            
-            .cornerRadius(5)
-            
-        }
-        
-        .frame(width: 280, height: 250)
-        .chartLegend(alignment:.bottomLeading,spacing: 20)
-        
-        .chartForegroundStyleScale(
-            range: [Color.DarkBlue, .lightblue, .navyBlue, .brandGreen])
-        .onAppear {
-            withAnimation(.smooth) {
-                isAnimating = true
-            }
-            
-            
-        }
-        .onDisappear {
-            isAnimating = false
-        }
-    }
-}
 
-#Preview {
-    DonutChartView(categoryTotals: [])
-}
 
 
 #Preview {
