becbaad6d41328d1dbfb3bd55a0dfd25a9ffcd0e
Benia Morgan <bmorganware@gmail.com>
2024-06-19 11:23:43 -0400

    Updated donut chart with dynamic data Removed hard coded data Added interactivity to donut chart so you can select a section of the chart for more information

--- SwiftDataTesting/Views/Charts/DonutChartView.swift
+++ SwiftDataTesting/Views/Charts/DonutChartView.swift
@@ -11,6 +11,9 @@ import Charts
 struct DonutChartView: View {
     let categoryTotals: [CategoryTotal]
     @State private var isAnimating = false
+    @State private var selectedCategory: CategoryTotal?
+    @State private var selectedCount: Double?
+
     
     var body: some View {
         ZStack {
@@ -18,28 +21,59 @@ struct DonutChartView: View {
                 SectorMark(
                     angle: .value("Total Spent", isAnimating == false ? 0 : item.total ),
                            innerRadius: .ratio(0.618),
+                           outerRadius: selectedCategory?.category == item.category ? 175 : 150,
                            angularInset: 1.5
                 )
                 .position(by: .value("Category", item.total))
                 .foregroundStyle(by: .value("Category", item.category.rawValue))
                 .cornerRadius(5)
+                .opacity(item.category == selectedCategory?.category ? 1.0 : 0.3)
                 
             }
             .padding(.bottom)
-            //.chartLegend(alignment:.leadingLastTextBaseline)
+            .chartLegend(.hidden)
+            .chartAngleSelection(value: $selectedCount)
             .chartForegroundStyleScale(
                 range: [Color.DarkBlue, .lightblue, .navyBlue, .brandGreen])
-        .animateOnAppear(isAnimating: $isAnimating)
+            .animateOnAppear(isAnimating: $isAnimating)
+            .onChange(of: selectedCount) { oldValue, newValue in
+                if let newValue {
+                    withAnimation {
+                        getSelectedCategory(value: newValue)
+                    }
+                }
+            }
+            
+        
             
             VStack {
-                Text("Category")
-                Text("Breakdown")
+                Text(selectedCategory?.category.rawValue ?? "Select a section")
+                    .bold()
+                if let selectedCategory {
+                    Text(currencyFormat(value:selectedCategory.total))
+                }
+                
             }
             .padding(.bottom)
             
         }
+       
     }
+    private func getSelectedCategory(value: Double) {
+        var cumulativeTotal = 0.0
+        let categoryName = categoryTotals.first { category in
+            cumulativeTotal += category.total
+            if value <= cumulativeTotal {
+                selectedCategory = category
+                return true
+            }
+            return false
+        }
+    }
+    
 }
+
+
 
 #Preview {
     DonutChartView(categoryTotals: [])
