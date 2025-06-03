adaacfa0531d74324d5417b4a749b0cec858d807
Benia Morgan <bmorganware@gmail.com>
2025-06-03 14:05:30 -0400

    finished func for YTD App Intent.

--- SwiftDataTesting/AppIntents/YTDAmountIntent/YearAmountIntent.swift
+++ SwiftDataTesting/AppIntents/YTDAmountIntent/YearAmountIntent.swift
@@ -13,15 +13,29 @@ struct YearAmountIntent: AppIntent {
     static var title: LocalizedStringResource = "YTD Spending"
     static var description: IntentDescription = "Check your year-to-date spending"
     
-    func perform() async throws -> some IntentResult {
+    @MainActor
+    func perform() async throws -> some IntentResult & ProvidesDialog & ReturnsValue<Double> {
         let modelContainer = try ModelContainer(for: Expense.self)
+        let currentDate: Date = Date()
+        let dateFormatter: DateFormatter = .init()
+        dateFormatter.dateFormat = "MMMM yyyy"
+        let currentMonthAndYear = dateFormatter.string(from: currentDate)
         
+        let expenseData = getYTDExpenseSumIntent(modelContext: modelContainer.mainContext, for: currentDate)
         
+        let totalSum = expenseData.values.reduce(0.0) {$0 + $1.sum }
         
+        if totalSum == 0 {
+            return .result(
+                value: 0.00,
+                dialog: "No expenses recorded this week"
+            )
+        }
+        let formattedSum = currencyFormat(value: totalSum)
         
         return .result(
-            value: "",
-            dialog: "Hello World"
+            value: totalSum,
+            dialog: "Your year-to-date spending as of \(currentMonthAndYear) is currently \(formattedSum)"
         )
 
     }
