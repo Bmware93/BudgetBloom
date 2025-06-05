b60e171e8a70ea10ae51d1833bfb259e4bb42d94
Benia Morgan <bmorganware@gmail.com>
2025-06-05 15:15:15 -0400

    created function to fetch all the expenses in the database and return a csv string with the formatted data.

--- SwiftDataTesting/Models/CSVGenerator.swift
+++ SwiftDataTesting/Models/CSVGenerator.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftData
+
+@MainActor
+func generateCSVFromExpenses(modelContainer: ModelContainer) throws -> String {
+    let modelContainer = try ModelContainer(for: Expense.self)
+    let fetchDescriptor = FetchDescriptor<Expense>()
+    let expenses = try modelContainer.mainContext.fetch(fetchDescriptor)
+    
+    let csvHeader = "Date, Name, Amount, Category, Description"
+
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    let csvBody: String = expenses.map {
+        "\(dateFormatter.string(from: $0.date)), \($0.name), \($0.amount), \($0.category.rawValue), \(escapeCSVField($0.expenseDescription))"
+    }.joined(separator: "\n")
+    
+    return csvHeader + "\n" + csvBody
+}
+
+func escapeCSVField(_ field: String) -> String {
+    if field.contains(",") || field.contains("\"") || field.contains("\n") {
+        let escapedField = field.replacingOccurrences(of: "\"", with: "\"\"")
+        return "\"\(escapedField)\""
+    }
+    return field
+}
