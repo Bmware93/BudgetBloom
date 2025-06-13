82fcdd31d1a6d62e3ddd3d403a2b6988ee01a659
Benia Morgan <bmorganware@gmail.com>
2025-06-13 10:18:48 -0400

    added styling and icons to the settings page along with captions.

--- SwiftDataTesting/Views/SettingsView.swift
+++ SwiftDataTesting/Views/SettingsView.swift
@@ -20,29 +20,84 @@ struct SettingsView: View {
             VStack {
                 List {
                     Section(header: Text("Share")) {
-                        Menu("Export Data", systemImage: "square.and.arrow.up") {
+                        Menu {
                             Button("All Transactions") {
                                 CSVExportManager.exportFromSwiftUI(expenses: expenses)
                             }
-                            
                             Button("Current Month only") {
                                 let currentMonthExpenses = getMonthlyExpenseSum(expenses: expenses, for: Date()).elements.first
                                 CSVExportManager.exportFromSwiftUI(expenses: currentMonthExpenses?.value.expenses ?? [])
                             }
-                            
                             Button("Year to date") {
                                 let ytdExpenses = getYTDExpenseSum(expenses: expenses, for: Date()).elements.values
                                 CSVExportManager.exportFromSwiftUI(expenses: ytdExpenses.flatMap(\.expenses))
                             }
+                        } label: {
+                            Label("Export Data", systemImage: "square.and.arrow.up")
                         }
                         .disabled(expenses.isEmpty)
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            VStack(alignment: .leading) {
+                                Label("Share", systemImage: "paperplane")
+                                
+                                HStack {
+                                    Text("Have a friend who'd love this app?")
+                                        .font(.footnote)
+                                }
+                                .padding(.leading, 46)
+                            }
+                            
+                        }
                     }
-                    
+                    Section(header: Text(LocalizedStringResource("Support"))) {
+                        NavigationLink {
+                            
+                        } label: {
+                            VStack(alignment:.leading) {
+                                Label("Tip Jar", systemImage: "giftcard")
+                                
+                                HStack {
+                                    Text("Support the developer's free side project!")
+                                        .font(.footnote)
+                                }
+                                .padding(.leading, 46)
+                            }
+                        }
+                        NavigationLink {
+                            
+                        } label: {
+                            VStack(alignment: .leading) {
+                                Label("Leave a Review", systemImage: "hand.thumbsup")
+                                HStack {
+                                    Text("Help others discover this app!")
+                                        .font(.footnote)
+                                }
+                                .padding(.leading, 46)
+                            }
+                        }
+                    }
+                    Section(header: Text(LocalizedStringResource("Help"))) {
+                        NavigationLink {
+                            
+                        } label: {
+                            VStack(alignment: .leading) {
+                                Label("Send Feedback", systemImage: "envelope")
+                                
+                                HStack {
+                                    Text("Email thoughts, bugs or questions.")
+                                        .font(.footnote)
+                                }
+                                .padding(.leading, 46)
+                            }
+                            
+                        }
+                    }
                 }
             }
             .navigationTitle("Menu")
-            
-            
         }
     }
 }
